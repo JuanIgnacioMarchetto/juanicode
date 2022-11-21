a807bf72b265dc51df45898519118418cbad20d0
JuanIgnacioMarchetto <43477348+JuanIgnacioMarchetto@users.noreply.github.com>
2022-11-21 18:07:32 -0300

    21/11/2022

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-// ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES //
 
-// LOGGING OUTPUT
 alert('Hello World'); // Do not use for debugging. Stops script and only strings
 console.log('Hello World');
 console.error('This is an error');
 console.warn('This is a warning');
 
 
-// VARIABLES - var, let, const
-let age = 30;
 
-// let can be re-assigned, const can not
+let age = 30;
 age = 31;
 
-
-// DATA TYPES - String, Number, Boolean, null, undefined
 const name = 'Brad';
 const age = 37;
 const rating = 3.5;
 const isCool = true;
 const x = null;
 const y = undefined;
-let z; // undefined
-
-// Check type
+let z; 
 console.log(typeof z);
 
 
